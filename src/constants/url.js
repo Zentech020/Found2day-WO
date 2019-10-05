0bd25685180eb413a0cf65fb5ba03721233b876b
Ysturkenboom <y.a.sturkenboom@gmail.com>
2019-10-05 20:55:28 +0200

    made backend origin env-dependent

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,3 +1,10 @@
-export const API_URL = 'https://found2day-wo.herokuapp.com';
+let backend_origin;
+if (process.env.ENV_TYPE === "PRODUCTION") {
+ backend_origin = 'https://found2day-wo.herokuapp.com';
+} else {
+ backend_origin = 'https://dev-found2day-wo.herokuapp.com';
+}
+
+export const API_URL = backend_origin;
 //http://127.0.0.1:5000
 //https://found2day-wo.herokuapp.com
